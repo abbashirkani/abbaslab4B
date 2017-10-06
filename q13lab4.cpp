9d17d0835bfe14be1b3f0bb77316792e78a77148
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-10-06 22:07:10 +0530

    Create q13lab4.cpp

---
+++ q13lab4.cpp
@@ -0,0 +1,23 @@
+# abbaslab4B
+// q 13
+
+
+#include <iostream>
+using namespace std;
+void disparr (int a[100],int n,int i)
+{if(i!=n)	
+{cout<<a[i]<<" ";
+disparr (a,n,i+1);
+}
+}
+
+int main() 
+{	int a[100],n,i;
+	cout<<"Enter the number of elements in the array ";
+	cin>>n;	cout<<"\nEnter the array elements ";
+	for(i=0;i<n;i++)
+	{cin>>a[i];}
+	cout<<"\nTHe elements of the array are ";
+	disparr(a,n,0);
+	return 0;
+}
