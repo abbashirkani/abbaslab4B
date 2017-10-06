5393a81bbb7a748527f754fef9acc33fa86cfc27
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-10-06 22:04:36 +0530

    Create q12lab4.cpp

---
+++ q12lab4.cpp
@@ -0,0 +1,25 @@
+# abbaslab4B
+// q 12
+
+
+#include <iostream>
+using namespace std;
+int lcm(int a,int b,int c)
+{
+if (b%a==0&&b%c==0)
+return b;
+else
+return lcm(a,b+1,c);}
+
+int main() 
+{	int a,b;
+	cout<<"Enter the numbers ";
+	cin>>a>>b;
+	if(a>b)	
+{cout<<"\nLCM of given numbers is "<<lcm(a,a,b);}
+	else if (b>a)
+	{cout<<"\nLCM is "<<lcm(a,b,b);}
+	else
+	cout<<"\n The LCM is "<<a;
+	return 0;
+}
